45e0d1d711d4e166518b1cce2f847d2ba1802c7f
Samir Butron <samirbut@hotmai.com>
2023-07-12 11:23:40 -0600

    SpringBoot API startup project

---
+++ src/main/java/com/mx/Incubation/IncubationApplication.java
@@ -0,0 +1,13 @@
+package com.mx.Incubation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IncubationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IncubationApplication.class, args);
+	}
+
+}
